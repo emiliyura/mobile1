b4bed87e6b0b249bee6df0375a6054295a1f5428
emiliyura <143761702+emiliyura@users.noreply.github.com>
2024-02-29 13:55:55 +0300

    Update NewDisplay.java

--- app/src/main/java/com/example/myapplication/NewDisplay.java
+++ app/src/main/java/com/example/myapplication/NewDisplay.java
@@ -1,22 +1,32 @@
-package com.example.myapplication;
+package com.example.mirea_prac_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.TextView;
 
-public class NewDisplay extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
+    // Переопределение метода onCreate, который вызывается при создании активности.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Вызов родительского метода onCreate для правильной инициализации активности.
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_display);
 
+        // Установка макета для текущей активности из файла ресурсов R.layout.activity_second.
+        setContentView(R.layout.activity_second);
+
+        // Получение Intent, который вызвал текущую активность.
         Intent i = getIntent();
+
+        // Извлечение строки сообщения из Intent с ключом "extra_message".
         String input_message = i.getStringExtra("extra_message");
-        TextView textView = findViewById(R.id.textView2);
-        textView.setTextSize(40);
-        textView.setText(input_message);
+
+        // Установка размера текста в 40 единиц для TextView с идентификатором R.id.textView.
+        ((TextView) findViewById(R.id.textView)).setTextSize(40);
+
+        // Установка текста в TextView с идентификатором R.id.textView равным извлеченному сообщению.
+        ((TextView) findViewById(R.id.textView)).setText(input_message);
     }
 }
