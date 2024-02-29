31db2825328540f56b62ec684e6fdc2388792eb4
emiliyura <143761702+emiliyura@users.noreply.github.com>
2024-02-29 13:55:36 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
-package com.example.myapplication;
+package com.example.mirea_prac_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,14 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, NewDisplay.class);
-        EditText editText = findViewById(R.id.editText);
+    // Объявление метода отправки сообщения, вызываемого при нажатии на кнопку или другой View.
+    public void sendMessage(View v) {
+        // Создание нового объекта Intent для перехода от текущей активности к SecondActivity.
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        // Получение ссылки на элемент EditText с идентификатором R.id.edittext.
+        EditText editText = findViewById(R.id.edittext);
+
+        // Извлечение текста из EditText и преобразование в строку.
         String message = editText.getText().toString();
+
+        // Добавление строки сообщения в Intent с ключом "extra_message".
         intent.putExtra("extra_message", message);
+
+        // Запуск активности SecondActivity с использованием Intent для передачи данных.
         startActivity(intent);
     }
+
 }
+
+
